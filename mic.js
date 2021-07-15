1a92a1ed4e4c8286d11c23aad4c04590f2d8497e
Brett Monty <fitnessnonsense@gmail.com>
2021-07-15 01:49:05 -0600

    Update mic.js

--- mic.js
+++ mic.js
@@ -1,11 +1,17 @@
 var mic;
 var active;
 var inactive;
+var slider;
+var output;
+var thresh = 0.005;
 
 function setup(){
 	createCanvas(700, 700);
 	mic = new p5.AudioIn();
 	mic.start();
+	slider = document.getElementById("slider")
+	slider.onInput = sliderMoved()
+	output = document.getElementById("val")
 	
 	active = loadImage('active.png');
 	//active.play();
@@ -14,13 +20,17 @@ function setup(){
 }
 
 function draw(){
-	
 	vol = mic.getLevel();
 	background(0, 255, 0);
 	image(inactive, 10, 10);
-	if (vol >= 0.003) {
+	if (vol >= thresh) {
 		image(active, 10, 10);
 	}
+}
+
+function sliderMoved() {
+	thresh = slider.value
+	output.innerHTML = thresh
 }
 
 function touchStarted() {
