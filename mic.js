d5daeff1946f7acbe3ee981e4039840528fdc496
Brett Monty <fitnessnonsense@gmail.com>
2021-07-15 02:22:07 -0600

    Update mic.js

--- mic.js
+++ mic.js
@@ -3,11 +3,28 @@ var active;
 var inactive;
 var thresh = 0.005;
 
+const getCookieValue = (name) => (
+  document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || ''
+)
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
 function setup(){
 	createCanvas(700, 700);
 	mic = new p5.AudioIn();
 	mic.start();
 	
+	if(getCookieValue("thresh") != "") {
+		thresh = parseFloat(getCookieValue("thresh"))
+	}
 	active = loadImage('active.png');
 	//active.play();
 	//active.loop();
@@ -31,4 +48,5 @@ function getSlider() {
 	var slider = document.getElementById("slider")
 	thresh = slider.value;
 	document.getElementById("val").innerHTML = thresh;
+	setCookie("thresh", slider.value, 15)
 }
