02fb20072a5e205b7d83a7bf5d72aa73d2768ad4
Brett Monty <fitnessnonsense@gmail.com>
2021-07-15 01:27:37 -0600

    Add files via upload

---
+++ mic.js
@@ -0,0 +1,22 @@
+var mic;
+var active;
+var inactive;
+function setup(){
+	createCanvas(700, 700);
+	mic = new p5.AudioIn();
+	mic.start();
+	
+	active = loadImage('active.png');
+	//active.play();
+	//active.loop();
+	inactive = loadImage('inactive.png');
+}
+function draw(){
+	
+	vol = mic.getLevel();
+	background(0, 255, 0);
+	image(inactive, 10, 10);
+	if (vol >= 0.000022) {
+		image(active, 10, 10);
+	}
+}
